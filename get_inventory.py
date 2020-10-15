50808cc8c477e4610034836699ad741954257ca4
Eric Gonzales <guardians611@gmail.com>
2020-10-15 09:50:27 -0400

    Add us-west-2

--- get_inventory.py
+++ get_inventory.py
@@ -7,7 +7,7 @@ print "Inventory profile: " + profile
 rds_count = 0
 ec2_count = 0
 lambda_count = 0
-regions = ["us-east-1", "us-east-2", "us-west-1", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-south-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "sa-east-1"]
+regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-south-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "sa-east-1"]
 
 for region in regions:
     print "[EC2] checking region " + region
